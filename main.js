651fc7ece9be16cd5fda230bb2b93a1f5006ba69
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-03-18 14:39:16 +0100

    esercizio generatore password

---
+++ main.js
@@ -0,0 +1,20 @@
+// var per memorizzare nome utente
+var nomeUtente;
+
+// domanda nome utente
+var nomeUtente = prompt("Inserire nome utente");
+
+// var per memorizzare cognome utente
+var cognomeUtente;
+
+// domanda cognome utente
+var cognomeUtente = prompt("Inserire cognome utente");
+
+// var per memorizzare colore preferito
+var colorePreferito;
+
+// domanda colore preferito
+var colorePreferito = prompt("Inserire colore preferito");
+
+// generazione password
+document.getElementById('mio-id').innerHTML = nomeUtente + cognomeUtente + colorePreferito + "19";
